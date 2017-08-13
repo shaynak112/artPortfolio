927854843f67dfe7c4e6f92726568bc104f20330
Shayna <shaynak112@gmail.com>
2017-08-13 18:07:07 -0400

    Add files via upload
    small changes to each part (nav menu, footer, index, unsure about header)

--- navMenuTop.php
+++ navMenuTop.php
@@ -1,6 +1,12 @@
 <div id='navMenuTop'>
 
-navigation menu placeholder
-
+<ul id='navList'>
+	<li class='navListItem'><a href='index.php'>Home</a></li>
+	<li class='navListItem'><a href='art.php'>Art</a></li>
+	<li class='navListItem'><a href='tattoos.php'>Tattoos</a></li>
+	<li class='navListItem'><a href='testimonials.php'>Testimonials</a></li>
+	<li class='navListItem'><a href='about.php'>About</a></li>
+	<li class='navListItem'><a href='contact.php'>Contact</a></li>
+</ul>
 
 </div>

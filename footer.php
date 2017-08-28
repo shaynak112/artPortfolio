0d547d742b54baae88ef9ae03c68a0d2b34ee087
Shayna <shaynak112@gmail.com>
2017-08-27 21:34:44 -0400

    Add files via upload
    adjusted nav menu header and general footer including nav section

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <footer id='footer'>
 <div class='row'>
-<div class='col-lg-6'>
+<div class='col-lg-8'>
 
 <div class='navMenuTop' id='navMenuFoot'>
 
 <ul class='navList' id='navListFoot'>
-	<li class='navListItem'><a href='index.php'>Home</a></li>
-	<li class='navListItem'><a href='art.php'>Art</a></li>
-	<li class='navListItem'><a href='tattoos.php'>Tattoos</a></li>
-	<li class='navListItem'><a href='testimonials.php'>Testimonials</a></li>
-	<li class='navListItem'><a href='about.php'>About</a></li>
-	<li class='navListItem'><a href='contact.php'>Contact</a></li>
+	<li class='navListItem navListItemFoot'><a href='index.php'>Home</a></li>
+	<li class='navListItem navListItemFoot'><a href='art.php'>Art</a></li>
+	<li class='navListItem navListItemFoot'><a href='tattoos.php'>Tattoos</a></li>
+	<li class='navListItem navListItemFoot'><a href='testimonials.php'>Testimonials</a></li>
+	<li class='navListItem navListItemFoot'><a href='about.php'>About</a></li>
+	<li class='navListItem navListItemFoot'><a href='contact.php'>Contact</a></li>
 </ul>
 
 </div><!--end navMenuTop-->
@@ -18,27 +18,21 @@
 </div><!--end col-lg-6-->
 
 
-<div class='col-lg-6'>
+<div class='col-lg-4'>
 
-<a href='mailto:davidjunk9@gmail.com'>Email: davidjunk9@gmail.com</a>
-
-<br/>
-
-<a href='#'>www.yourwebsite.com</a>
+<a href='mailto:tatttodavidjunk@gmail.com'>Email: tatttodavidjunk@gmail.com</a>
 
 <br/>
 
-&copy; David Junk, <script>thisyear()</script>
+<a href='www.davidjunk.com'>www.davidjunk.com</a>
+
+<br/>
+
+&copy; David Junk, 2017
 
 </div><!--end col-lg-6-->
 </div><!--end row-->
 
 </footer>
-
-<script>
-
-/*blink function for "top nine" images only on homepage all at different speeds*/
-
-</script>
 
 

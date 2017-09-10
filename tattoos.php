b200aa5d38c2413ce60af547572c5bbfc3659dd3
Shayna <shaynak112@gmail.com>
2017-09-10 07:40:02 -0400

    Add files via upload
    updated to rotate all three images on homepage; still some work to do. also changed logo so it was inline with the nav menu

--- tattoos.php
+++ tattoos.php
@@ -20,32 +20,65 @@
 
 <p><strong>I work in fully sterile locations and use only qualitative paints and tools that are guaranteed. I can work in your home or place of your choice or you can come to my home.</strong></p>
 <p style='text-align:center;'>Tattoo Sketches: $50</p>
+<p style='text-align:center;'><a href='galleryTattoos.php'>View the Gallery</a></p>
 
 <br/>
 
-<img src='images/tattoos/img_04.jpg' style='width:30%;margin-left:10%;margin-right:5%;'>
+</div><!--close col-lg-8-->
+</div><!--close row-->
+
+
+
+<div class='row'>
+<div class='col-lg-8 col-lg-offset-2'>
+
+	<div class='col-lg-3'>
+		<img src='images/gallery/tattoos/img_242.jpg' style='width:70%;margin-left:15%;'>
+	</div>
+
+	<div class='col-lg-3'>
+		<img src='images/gallery/tattoos/img_246.jpg' style='width:70%;margin-left:15%;'>
+	</div>
+
+	<div class='col-lg-3'>
+		<img src='images/gallery/tattoos/img_232.jpg' style='width:70%;margin-left:15%;'>
+	</div>
+
+	<div class='col-lg-3'>
+		<img src='images/gallery/tattoos/img_243.jpg' style='width:70%;margin-left:15%;'>
+	</div>
+
+
+
+</div><!--close col-lg-8-->
+</div><!--close row-->
+
+<!--<img src='images/tattoos/img_04.jpg' style='width:30%;margin-left:10%;margin-right:5%;'>
 <img src='images/tattoos/img_02.jpg' style='width:30%;margin-left:5%;margin-right:5%;'>
 
-<button id='btnTattoosMore' style='background-color:black;'>More</button>
+<button id='btnTattoosMore' style='background-color:black;'>Preview</button>
 
 <div class='modal' id='modalTattoosMore'>
 	<div class='modalContent'>
 	    <span class="close">&times;</span>
 
 	<?php
-		include 'moreTattoos.php';
+		//include 'moreTattoos.php';
 
 	?>
 
 
 
 	</div>
-</div>
+</div>-->
+
+<br/>
+<br/>
 
 
 
-</div><!--end div col-lg-8-->
-</div><!--end row-->
+
+
 
 <?php
 	include 'footer.php';

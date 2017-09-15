e3621162412efe8ecaed2039c6314f2e39ccfcae
Shayna <shaynak112@gmail.com>
2017-09-15 00:51:20 -0400

    Add files via upload
    adjusted footer

--- footer.php
+++ footer.php
@@ -1,16 +1,45 @@
-<footer id='footer'>
+<footer class='footer'>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
 <div class='row'>
 <div class='col-lg-8'>
 
 <div class='navMenuTop' id='navMenuFoot'>
 
 <ul class='navList' id='navListFoot'>
+
+
+	<li class='navListItem navListItemFoot'><a href='contact.php'>Contact</a></li>
+<li class='navListItem navListItemFoot'><a href='about.php'>About</a></li>
+<li class='navListItem navListItemFoot'><a href='testimonials.php'>Testimonials</a></li>
+<li class='navListItem navListItemFoot'><a href='tattoos.php'>Tattoos</a></li>
+<li class='navListItem navListItemFoot'><a href='canvas.php'>Canvas</a></li>
+
+
+		<li class='navListItem navListItemFoot'><a class='treeview' href='galleries.php'>Galleries</a>
+			<ul>
+			<li class='galleryList navListItem navListItemFoot'><a href='galleryTattoos.php'>Tattoos</a></li><br/>
+
+			<li class='galleryList navListItem navListItemFoot'><a href='galleryCanvas.php'>Canvas</a></li><br/>
+			
+			<li class='galleryList navListItem navListItemFoot'><a href='galleryWeb.php'>Web</a></li>
+			</ul>
+		</li>
+			
+
 	<li class='navListItem navListItemFoot'><a href='index.php'>Home</a></li>
+
+	<!--<li class='navListItem navListItemFoot'><a href='index.php'>Home</a></li>
 	<li class='navListItem navListItemFoot'><a href='art.php'>Art</a></li>
 	<li class='navListItem navListItemFoot'><a href='tattoos.php'>Tattoos</a></li>
 	<li class='navListItem navListItemFoot'><a href='testimonials.php'>Testimonials</a></li>
 	<li class='navListItem navListItemFoot'><a href='about.php'>About</a></li>
-	<li class='navListItem navListItemFoot'><a href='contact.php'>Contact</a></li>
+	<li class='navListItem navListItemFoot'><a href='contact.php'>Contact</a></li>-->
 </ul>
 
 </div><!--end navMenuTop-->
